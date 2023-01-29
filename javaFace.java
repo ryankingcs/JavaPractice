f51f032aad14f42df5bdcae1c695e188144f75d5
Ryan King <ryan.king2016@loretomilford.net>
2023-01-29 22:34:37 +0000

    Add files via upload

---
+++ javaFace.java
@@ -0,0 +1,17 @@
+package javapractice2901;
+
+public class javaFace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" +'''''+ ");
+		System.out.println("[| o o |]");
+		System.out.println(" |  ^  |");
+		System.out.println(" | '-' |");
+		System.out.println(" +-----+ ");
+		
+
+	}
+
+}
