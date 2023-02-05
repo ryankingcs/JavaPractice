1caa1a69244e35831ff40151c47998774da0265c
Ryan King <ryan.king2016@loretomilford.net>
2023-02-05 17:57:26 +0000

    Add files via upload

---
+++ quiz21321321.java
@@ -0,0 +1,23 @@
+package javapractice;
+
+public class quiz21321321 {
+
+	public static void main(String[] args) {
+		int option =2;
+		String output = "";
+		switch(option) {
+		case 1:
+			output = "A";
+		case 2:
+			output = "b";
+		case 3:
+			output = "c";
+		case 4:
+			output = "d";
+			default:
+				output = "e";
+			
+		}
+		System.out.println(output);
+}
+}
